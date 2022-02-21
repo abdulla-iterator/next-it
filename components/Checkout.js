e198ef0aba456318a64a33ef3436d3ba89235cd6
= <=>
2022-02-21 20:08:03 +0530

    pay-order

--- components/Checkout.js
+++ components/Checkout.js
@@ -8,7 +8,13 @@ import {
 } from '@stripe/react-stripe-js';
 import nProgress from 'nprogress';
 import { useState } from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+import { useRouter } from 'next/dist/client/router';
 import SickButton from './styles/SickButton';
+import { useCart } from '../lib/cartState';
+import { GET_CURRENT_USER } from './User';
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -18,7 +24,19 @@ const CheckoutFormStyles = styled.form`
   grid-gap: 1rem;
   display: grid;
 `;
-
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    checkout(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        name
+      }
+    }
+  }
+`;
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
 function CheckoutForm() {
@@ -26,6 +44,15 @@ function CheckoutForm() {
   const [loading, setLoading] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
+  const router = useRouter();
+  const { closeCart } = useCart();
+
+  const [checkout, { error: graphQLError }] = useMutation(
+    CREATE_ORDER_MUTATION,
+    {
+      refetchQueries: [{ query: GET_CURRENT_USER }],
+    }
+  );
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -39,10 +66,23 @@ function CheckoutForm() {
     console.info(paymentMethod);
     if (error) {
       setError(error);
+      nProgress.done();
+      return; // stop  checkout
     }
     //  send the token from step 3 to our keystone server, via a custom mutation!
+    const order = await checkout({
+      variables: {
+        token: paymentMethod.id,
+      },
+    });
+    console.log(order);
     //  change the page to view the order
+    router.push({
+      pathname: `/order/[id]`,
+      query: { id: order.data.checkout.id },
+    });
     // close the cart
+    closeCart();
 
     setLoading(false);
     nProgress.done();
@@ -51,6 +91,8 @@ function CheckoutForm() {
   return (
     <CheckoutFormStyles onSubmit={handleSubmit}>
       {error && <p style={{ fontSize: 12 }}>{error.message}</p>}
+      {graphQLError && <p style={{ fontSize: 12 }}>{graphQLError.message}</p>}
+
       <CardElement />
       <SickButton>Check Out Now</SickButton>
     </CheckoutFormStyles>
